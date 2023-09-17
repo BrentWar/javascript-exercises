9b98623cdef414c50012505e747a6a27d161cce1
BrentWar <142748469+BrentWar@users.noreply.github.com>
2023-09-17 13:00:41 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
 
+    function reducer(curOld, person) {
+        let birthYear = person.yearOfBirth;
+        let deathYear = 0;
+        let olderDeathYear = 0;
+        let olderBirthYear = curOld.yearOfBirth;
+        if (!("yearOfDeath" in person)) {
+            let today = new Date();
+            deathYear = today.getFullYear();
+        } 
+        else {
+            deathYear = person.yearOfDeath;
+        }
+
+        if (!("yearOfDeath" in curOld)) {
+            let today = new Date();
+            olderDeathYear = today.getFullYear();
+        } 
+        else {
+            olderDeathYear = curOld.yearOfDeath;
+        }
+
+        if (olderDeathYear - olderBirthYear < deathYear - birthYear) {
+            return person;
+        } 
+        else {
+            return curOld;
+        }
+    }
+
+    return a.reduce(reducer);
 };
 
 // Do not edit below this line
