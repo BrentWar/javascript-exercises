de3644e308274bec3621b18136816e8bf23347ef
BrentWar <142748469+BrentWar@users.noreply.github.com>
2023-09-16 21:19:02 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a < 1) {
+        return "OOPS";
+    }
+    else if (a == 1 || a == 2) {
+        return 1;
+    }
+    else {
+        return fibonacci(a - 1) + fibonacci(a - 2);
 
+    }
 };
 
 // Do not edit below this line
