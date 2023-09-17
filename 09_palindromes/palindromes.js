9c22fa983766b2a79c615c81e9c481051b32ffa6
BrentWar <142748469+BrentWar@users.noreply.github.com>
2023-09-16 21:08:18 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    let lowercase = a.toLowerCase();
+    let arrayA = lowercase.split("");
+    arrayA = arrayA.filter(element => (element !== "!" && element !== " " && element !== "," && element !== "."));
+    let palindrome = true;
+    for (let i = 0; i < Math.floor(arrayA.length / 2); ++i) {
+        if (arrayA[i] !== arrayA[arrayA.length - 1 - i]) {
+            palindrome = false;
+        }
+    }
+
+    return palindrome;
 
 };
 
